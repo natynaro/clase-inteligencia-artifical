9f9694eab04e8578d4445eeb44a027b9b08fc6bc
andre <andres.hoyos18@eia.edu.co>
2024-07-17 15:28:37 -0500

    .

---
+++ busqueda-ciegas-net/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace busqueda_ciegas_net;
+
+public class Node
+{
+    public object State { get; private set; }
+    public object Value { get; private set; }
+    public List<Node> Children { get; private set; } = [];
+    public Node? Parent { get; set; }
+    public OperatorDelegate? Operator { get; private set; }
+    public List<OperatorDelegate> Operators { get; private set; } = [];
+    public object Objective { get; private set; }
+    public int Level { get; private set; }
+
+    public delegate Node OperatorDelegate(Node node);
+}
